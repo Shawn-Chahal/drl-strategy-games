42137fe6827a749a9d074ba180f9f9e7cd8228e7
Shawn-Chahal <shawn.chahal@gmail.com>
2020-10-04 19:03:00 -0400

    Continued training Reversi

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     game = drl.Reversi()
     mode = 'train'  # 'train' or 'play'
     plot_only = False
-    initial_epoch = 348
+    initial_epoch = 386
     final_epoch = 1000
 
     PROCESSES = 5
